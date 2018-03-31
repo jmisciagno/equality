15426d752d478509cadc2f92fe965af6c2243615
jmisciagno <misciag2@uwm.edu>
2018-03-31 23:33:16 +0000

    Create equality.v

---
+++ equality.v
@@ -0,0 +1,13 @@
+Definition eq' {A} (x y : A) : Prop :=
+  forall (f : A -> Prop), f x <-> f y.
+
+Theorem eq'_is_eq {A} : forall (x y : A), eq x y <-> eq' x y.
+  intros.
+  split.
+  intro H.
+  rewrite <- H.
+  firstorder.
+  intro H.
+  apply (H (eq x)).
+  reflexivity.
+Defined.
